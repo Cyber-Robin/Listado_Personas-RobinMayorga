fcbd778fbce7f4e3279564e803bf8290ddcd216b
Cyber-Robin <petirrojo.9@gmail.com>
2022-10-12 15:59:46 -0600

    1er, commit

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { LoggingService } from '../LoggingService.service';
+import { Persona } from '../persona.model';
+import { PersonasService } from '../persona.service';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css'],
+})
+export class FormularioComponent {
+  //@Output() personaCreada = new EventEmitter<Persona>();
+
+  nombreInput: string;
+  apellidoInput: string;
+
+  constructor(
+    private loggingService: LoggingService,
+    private personaService: PersonasService
+  ) {}
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    //this.loggingService.enviaMensajeAConsola('Enviamos persona con nombre:' + persona1.nombre + ', apellido:' + persona1.apellido);
+    //this.personaCreada.emit(persona1);
+    this.personaService.agregarPersona(persona1);
+  }
+}
---
+++ src/app/persona.model.ts
@@ -0,0 +1,15 @@
+export class Persona{
+    /*
+    nombre: string = '';
+    apellido: string = '';
+
+    constructor(nombre: string, apellido: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    */
+
+    constructor( public nombre: string, public apellido: string){}
+
+}
+// ES LA CLASE PERSONA CON EL CONSTRUCTOR QUE ENVIA LOLS PARAMETROS/
